b26a2ed301673aec9e95511166f1481c4aa825d2
zhang00 <wb-zll546111@antfin.com>
2019-11-18 16:48:16 +0800

    继续修改侧边栏配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,7 +7,9 @@ module.exports = {
                 collapsable: false, // 可选的, 默认值是 true,
                 sidebarDepth: 1,    // 可选的, 默认值是 1
                 children: [
-                    ['/b.md','b.md']
+                    '/',
+                    ['/b.md','b.md 1234'],
+                    ['/test/a.md', 'a.md']
                 ]
             },
             // {
