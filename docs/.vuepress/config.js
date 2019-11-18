a74a41bc70fa7d260b49baebec439c7e8c49df19
zhang00 <wb-zll546111@antfin.com>
2019-11-18 16:31:27 +0800

    添加 vuepress，配置基础侧边栏

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    themeConfig: {
+        sidebar: [
+            {
+                title: 'Group 1',   // 必要的
+                path: '/foo/',      // 可选的, 应该是一个绝对路径
+                collapsable: false, // 可选的, 默认值是 true,
+                sidebarDepth: 1,    // 可选的, 默认值是 1
+                children: [
+                    ['/b.md','b.md']
+                ]
+            },
+            // {
+            //     title: 'Group 2',
+            // }
+        ]
+    }
+}
